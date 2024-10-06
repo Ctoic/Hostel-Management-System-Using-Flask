688e341802e44e2c7eb2bbbce25919cd3b0dea75
Jaid101 <jaidtamboli101@gmail.com>
2024-10-07 00:43:19 +0530

    I add the Home page
    I add the home page and make the home page more interactive . If you want to design remaining pages then assign me that pages I will work onit

--- app.py
+++ app.py
@@ -24,8 +24,32 @@ def create_tables():
     db.session.commit()
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+# Student Login
+@app.route('/student_login')
+def student_login():
+    return render_template('student_login.html')
+
+# Admin Login
+@app.route('/admin_login')
+def admin_login():
+    return render_template('admin_login.html')
+
+# Room Management (Admin Only)
+@app.route('/room_management')
+def room_management():
+    return render_template('room_management.html')
+
+# Student Registration
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 @app.route('/enroll', methods=['GET', 'POST'])
 def enroll():
